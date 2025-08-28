256720be9b3da1d83284881224fbcd041d92490e
Ivo Mariezcurrena <Ivomariezcurrena@hotmail.com>
2025-08-28 02:27:26 -0300

    Index listo responsive

--- Sprint-1-2/home/script.js
+++ Sprint-1-2/home/script.js
@@ -8,6 +8,16 @@ const productosDestacados = [
         nombre: "Sillas Córdoba",
         imagen: "../img/Sillas Córdoba.png",
         descripcion: "Set de cuatro sillas apilables en contrachapado moldeado de nogal y estructura tubular pintada en Sage Green."
+    },
+    {
+        nombre: "Silla de trabajo Belgrano",
+        imagen: "../img/Silla de trabajo Belgrano.png",
+        descripcion: "Silla ergonómica regulable en altura con respaldo de malla transpirable y asiento tapizado en tejido reciclado."
+    },
+    {
+        nombre: "Escritorio Costa",
+        imagen: "../img/Escritorio Costa.png",
+        descripcion: "Escritorio compacto con cajón organizado y tapa pasacables integrada en bambú laminado. "
     }
 ];
 
@@ -20,15 +30,18 @@ productosDestacados.forEach(producto => {
     img.src = producto.imagen;
     img.alt = producto.nombre;
 
+    const sliceTexto = document.createElement('div');
+    sliceTexto.classList.add('slice-texto');
+
     const nombre = document.createElement('h3');
     nombre.textContent = producto.nombre;
 
     const descripcion = document.createElement('p');
     descripcion.textContent = producto.descripcion;
 
+    sliceTexto.appendChild(nombre);
+    sliceTexto.appendChild(descripcion);
     productoDiv.appendChild(img);
-    productoDiv.appendChild(nombre);
-    productoDiv.appendChild(descripcion);
-
+    productoDiv.appendChild(sliceTexto);
     contenedor.appendChild(productoDiv);
 });
